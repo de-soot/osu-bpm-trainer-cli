e958bedd683483a674ff32473aa7d3ab89d8d704
dE_soot <78423238+LeSoot@users.noreply.github.com>
2023-12-09 00:39:35 +0800

    nvm just keep the tests simple

--- tests/sound/main.c
+++ tests/sound/main.c
@@ -5,7 +5,7 @@
 #include <mmsystem.h>
 
 int main(void) {
-    PlaySound(TEXT("metronome.wav"), NULL, SND_FILENAME | SND_ASYNC);
+    PlaySound(TEXT("metronome.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
     _getch();
 
     return 0;
